02de9ceccee6d442c0459067cdf19d5c45bfaf92
Lucas Tavares <tavares_lucas@hotmail.com>
2018-10-26 17:32:16 -0200

    Challenge Lucas Tavares Dev

---
+++ dist/precache-manifest.89239c25c307e85762f894acc87d781c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70e2ac49d2049a0a7d3e",
+    "url": "/static/js/main.70e2ac49.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "70e2ac49d2049a0a7d3e",
+    "url": "/static/css/main.871ef9e5.chunk.css"
+  },
+  {
+    "revision": "246ceeec875fb15aa987d8f0c763e3d9",
+    "url": "/index.html"
+  }
+];
