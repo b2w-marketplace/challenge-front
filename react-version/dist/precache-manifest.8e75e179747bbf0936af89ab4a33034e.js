44cebf2b0aaf1c9dc57774677e1c4400037d9559
michelonsouza <michelon.souza@hotmail.com>
2018-11-14 05:10:07 -0200

    Finalizando a tarefa com projetos em Vue e React

---
+++ react-version/dist/precache-manifest.8e75e179747bbf0936af89ab4a33034e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6eaaa72c398756b5b61",
+    "url": "/static/js/main.a6eaaa72.chunk.js"
+  },
+  {
+    "revision": "7784d69c59c748b54d2f",
+    "url": "/static/js/1.7784d69c.chunk.js"
+  },
+  {
+    "revision": "a6eaaa72c398756b5b61",
+    "url": "/static/css/main.63d1b9fe.chunk.css"
+  },
+  {
+    "revision": "b620fbab3e85b2027fcda6615842a1d5",
+    "url": "/index.html"
+  }
+];
