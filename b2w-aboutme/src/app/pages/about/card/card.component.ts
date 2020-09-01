42351c005c677a53229ea5e363769e5f36d37905
Raphael S. Bezerra <raphaelbzr@gmail.com>
2020-09-01 02:12:49 -0300

    personalizado componente de card e ajustado css responsivo

--- b2w-aboutme/src/app/pages/about/card/card.component.ts
+++ b2w-aboutme/src/app/pages/about/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'b2w-card',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() buttonTitle: string;
 
   moreInfo = false;
 
